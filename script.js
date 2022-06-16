c798ce8bbc01afc628a14fda277de18733847d4b
Cindy <95900449+cintabile@users.noreply.github.com>
2022-06-16 21:26:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 alert("來~歡迎光臨這邊坐喔");
-var user_name = prompt("來手伸出來，你叫什麼名字:");
+var user_name = prompt("來~手伸出來，告訴阿姨你叫什麼名字:");
 alert(user_name + "有沒有人跟你說過，你這種手很好命喔...");
-var nail_service = confirm("阿姨幫你指甲彩繪齁[Y/N]")
+var nail_service = confirm("阿姨幫你指甲彩繪齁[Cancel/OK]")
 if(nail_service){
     alert("(你不情願地等阿姨蹂躪完你的指甲)") 
 } else{
-    alert("什麼你沒有要指甲彩繪唷？") 
+    alert("什麼，你沒有要指甲彩繪唷？") 
 }
 function ask_your_age(){
 var user_age = prompt("好啦，西元幾年出生:");
